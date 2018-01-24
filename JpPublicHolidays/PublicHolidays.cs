40acc9a4de51a4ce46be91a3dc1e6107b6a875f5
Kiyoaki Tsurutani <kiyoaki.tsurutani@gmail.com>
2018-01-24 10:29:12 +0900

    Add TargetFramework NET45

--- JpPublicHolidays/PublicHolidays.cs
+++ JpPublicHolidays/PublicHolidays.cs
@@ -19,7 +19,9 @@ namespace JpPublicHolidays
 
         static PublicHolidays()
         {
+#if !NET45
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+#endif
         }
 
         public static async Task<Holiday[]> Get()
