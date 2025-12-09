b0e2cdf21143876222eb4cea77c110b097ec7e52
Kiyoaki Tsurutani <kiyoaki.tsurutani@gmail.com>
2025-12-09 23:19:29 +0900

    Update dependencies and test project configuration
    Upgraded CsvHelper and System.Text.Encoding.CodePages in the main project. Updated NextUnit in the test project, changed test project output type to Library, and removed the obsolete Program.cs file.

--- tests/JpPublicHolidays.Test/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.Testing.Platform.Builder;
-using NextUnit.Platform;
-
-var builder = await TestApplication.CreateBuilderAsync(args);
-builder.AddNextUnit();
-using var app = await builder.BuildAsync();
-return await app.RunAsync();
